06ac1df012ba8f00a3b9f80bc958bbf27fe449b9
Hernan <hernan.malatini@mercadolibre.com>
2017-03-14 14:55:28 -0300

    for pull

--- grails-app/controllers/com/meli/academy/grails/geo/GeoposicionController.groovy
+++ grails-app/controllers/com/meli/academy/grails/geo/GeoposicionController.groovy
@@ -9,11 +9,8 @@ class GeoposicionController {
 		println "Por lo que se ve, este controller no hace un carajo"
 		println "Facu sos un garca, no hiciste nada"
 		println ("modificacion de facu");
-<<<<<<< HEAD
 		println ("modificacion de cou zarpada");
-=======
 	 	def msg = "dejen de hacer cagada"
 		println (msg);
->>>>>>> 85c75fdb3a9b9208768e69ae0d284d4e3f5419df
 	}
 }
