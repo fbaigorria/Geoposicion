2ec56cd50350dfae2deccb2e2c4f205cc9d14bce
hernan.malatini@mercadolibre.com <hmalatini@BRSP023227.local>
2017-03-14 13:22:32 -0300

    Modifique el controller HERNAN

--- grails-app/controllers/com/meli/academy/grails/geo/GeoposicionController.groovy
+++ grails-app/controllers/com/meli/academy/grails/geo/GeoposicionController.groovy
@@ -5,6 +5,7 @@ class GeoposicionController {
     def index() { }
 	
 	def getGeoposicion() {
-		
+		println "Por lo que se ve, este controller no hace un carajo"
+		println "Facu sos un garca, no hiciste nada"
 	}
 }
